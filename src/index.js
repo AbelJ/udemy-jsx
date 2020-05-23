59b0f94dbd3a971baa080a1a5fe5ebfbffd0b7f1
AbelJ <abelmendivil@gmail.com>
2020-05-24 11:30:26 +1200

    Section 3 - Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 // import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>
+
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                Are you sure you want to do this?
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail  author={Faker.name.findName()} 
+                                avatar={Faker.image.avatar()}
+                                content={Faker.lorem.sentence()}
+                                timeAgo="Today 3:45" />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail  author={Faker.name.findName()} 
+                                avatar={Faker.image.avatar()}
+                                content={Faker.lorem.sentence()}
+                                timeAgo="Today 2:30" />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail  author={Faker.name.findName()} 
+                                avatar={Faker.image.avatar()}
+                                content={Faker.lorem.sentence()}
+                                timeAgo="Yesterday 9:45" />
+            </ApprovalCard>
+        </div>
+    );
 };
 
 // Take a react component and show it on the screen
